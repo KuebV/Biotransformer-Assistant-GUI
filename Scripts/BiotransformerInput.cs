e2128f2a1c27cde062519aae23b59fcb1bafaf9c
KuebV <42746052+KuebV@users.noreply.github.com>
2022-02-07 19:43:27 -0700

    Add files via upload

---
+++ Scripts/BiotransformerInput.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiotransformerInput
+{
+    public string ListName { get; set; }
+    public string MetabolismType { get; set; }
+    public string FileType { get; set; }
+    public string BiotransformerFileName { get; set; }
+    
+}
---
+++ Scripts/SkippedCompoundsListElement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SkippedCompoundsListElement : MonoBehaviour
+{
+    public TMP_Text Index;
+    public TMP_InputField CompoundName;
+    public TMP_InputField SMILE;
+    public int IndexInt;
+}
